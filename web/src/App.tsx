7823ac4fd4e26eba1ca548fa511b86c093694c06
Lucas Vidotto <lucasvidotto3@gmail.com>
2023-01-19 13:24:12 -0300

    aula 02 web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
-
+import { Header } from './Components/Header'
+import { SummeryTable } from './Components/SummaryTable'
 import './Styles/global.css'
-import logoImage from './assets/logo.svg'
+
 export function App() {
 
 
@@ -8,10 +9,8 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
       
-      <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-        <img src={logoImage} alt="Imagem Logo" />
-        <button type="button">Novo habito</button>
-      </div>
+      <Header />
+      <SummeryTable />
 
       </div>
     </div>
---
+++ web/src/Components/SummaryTable.tsx
@@ -0,0 +1,48 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
+import { HabitDay } from "./HabitDay"
+
+const weekDays =[
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S',
+]
+
+const summarryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7 
+const amountDaysToFill = minimumSummaryDatesSize - summarryDates.length
+
+export function SummeryTable(){
+    return(
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                {weekDays.map((weekDay,i) =>{
+                    return(
+                        <div 
+                            key={`${weekDay}-${i}`} 
+                            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+                            {weekDay}
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                {summarryDates.map(date=>{
+                    return <HabitDay key={date.toString()}/>
+                })}
+
+                {amountDaysToFill > 0 && Array.from({length: amountDaysToFill}).map((_,i)=>{
+                   return  
+                   <div 
+                    key={i}
+                    className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+                })}
+            </div>
+        </div>
+    )
+}
